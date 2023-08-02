590dd5c5d7502900b9ecf48177806434c94243bf
Gary Garcia <138717926+ggarciacruz@users.noreply.github.com>
2023-08-02 05:24:41 -0400

    solucion a lf crlf

---
+++ exercise-caller.js
@@ -0,0 +1,13 @@
+//@ts-check
+//En este archivo se hacen todas las llamadas a los ejercicios realizados
+
+const basicExercises  = require("./exercises/basic-exercices");
+const intermediateExercises = require("./exercises/intermediate-exercises");
+const advancedExercises = require("./exercises/advanced-exercises");
+
+//console.log(basicExercises.fromCelciusToFahrenheit(32));
+//console.log(intermediateExercises.palindromo('anita lava la tina'));
+//console.log(intermediateExercises.duplicateElement([1,2,3,4,5,6,7,8,8]));
+console.log(advancedExercises.sumEquals([10,6,1,1,1,2,1,100],[10,100,1,1,1,2,6]));
+
+
---
+++ exercises/clases-objetos.js
@@ -0,0 +1,55 @@
+
+
+/**
+ * Clase celular
+ */
+class Celular{
+    /**
+     * Contructor de la clase celular
+     * @param {string} marca Se debe ingresar la marca del celular
+     * @param {String} modelo Se debe ingresar el modelo del celular
+     * @param {String} camara Se debe ingresar el tipo de camara del elular
+     */
+    constructor(marca, modelo, camara){
+        this.marca=marca;
+        this.modelo=modelo;
+        this.camara=camara;
+
+    }
+
+     call() {
+        return ('estoy haciendo un llamado desde mi cel:'+ this.modelo);
+    }
+
+}
+
+
+/**
+ * Clase estudiante
+ */
+class Estudiante{
+    /**
+     * 
+     * @param {string} nombre Se debe ingresar el nombre del estudiante
+     * @param {string} edad Se debe ingresar la edad del estudiante
+     * @param {string} grado Se debe ingresar el grado del estudiante
+     */
+    constructor(nombre, edad, grado){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.grado=grado;
+    }
+
+    /**
+     * retorna que el estudiante esta estudiando
+     */
+    estudiar(){
+        return ('el estudiante '+this.nombre+', esta estudiando.');
+    }
+}
+
+
+module.exports={
+    Estudiante: Estudiante,
+    Celular:Celular
+}
